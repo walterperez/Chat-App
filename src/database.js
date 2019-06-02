8450539a37f5b215457871cf3428bb6a6e0b66bd
Walter Perez <waltermaniaco@gmail.com>
2019-06-02 23:21:39 +0200

    aded conection to mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
-const config = require('config');
+const config = require("config");
 
 // DB Config
-const db = config.get('mongoURI');
+const db =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : config.get("mongoURI");
 
-mongoose.connect(db, { 
-        useNewUrlParser: true
-    })
-    .then(console.log("Database is connected"))
-    .catch(err => {console.log(`Database error: ${err}`)});
+mongoose
+  .connect(db, {
+    useNewUrlParser: true
+  })
+  .then(console.log("Database is connected"))
+  .catch(err => {
+    console.log(`Database error: ${err}`);
+  });
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const morgan = require("morgan");
+require("dotenv").config();
 
 //Import Routes
 const indexRoute = require("./routes/api/indexRoute");
@@ -13,7 +14,7 @@ const roomRoute = require("./routes/api/roomRoute");
 
 //Database & Configs
 require("./database");
-require("./helpers/passport")
+require("./helpers/passport");
 
 // Socket.io
 const SocketIO = require("socket.io");
@@ -22,44 +23,45 @@ const SocketIO = require("socket.io");
 app.set("PORT", process.env.PORT || 3000);
 
 //Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(session({
-    secret: '0-Chat-1-App-2-Secret-3-ID-4',
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "0-Chat-1-App-2-Secret-3-ID-4",
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //Routes
-app.use("/api/",indexRoute);
-app.use("/api/user",userRoute);
-app.use("/api/room:id",roomRoute);
+app.use("/api/", indexRoute);
+app.use("/api/user", userRoute);
+app.use("/api/room:id", roomRoute);
 
 //Static Files
 // app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Start Express Server
-const server = app.listen(app.get("PORT"), ()=>{
-    console.log(`Express Server running on port: ${app.get("PORT")}`);
-})
+const server = app.listen(app.get("PORT"), () => {
+  console.log(`Express Server running on port: ${app.get("PORT")}`);
+});
 
 //Socket IO
-const io = SocketIO(server)
+const io = SocketIO(server);
 
-io.on("connection", (socket)=>{
-    
-    console.log(`New Connected User: ${socket.id}`)
+io.on("connection", socket => {
+  console.log(`New Connected User: ${socket.id}`);
 
-    socket.on("chat:message",(data)=>{
-        io.sockets.emit("chat:message",data);
-    })
+  socket.on("chat:message", data => {
+    io.sockets.emit("chat:message", data);
+  });
 
-    socket.on("chat:typing",(username)=>{
-       socket.broadcast.emit("chat:typing",username);
-    })
-})
+  socket.on("chat:typing", username => {
+    socket.broadcast.emit("chat:typing", username);
+  });
+});
